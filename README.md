5c0cb18041a6deabced1379f52034af1ca461c28
Martin Buessemeyer <martin.buessemeyer@student.hpi.de>
2021-01-22 17:41:32 +0100

    add linter, recommended plugins, scss and replace js with typescript

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Install dependencies: `npm install`
 Build Website: `npm run build`
 
 Run Website: `npm start`
+
+### Linting
+
+Test for warnings (return non zero exit code if linting printed warnings): - `npx eslint ./src --ext .js,.jsx,.ts,.tsx --max-warnings 0`
+
+Autofix linter problems: `npx eslint ./src/ --ext .js,.jsx,.ts,.tsx --fix`
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,68 @@
-'use strict'
+'use strict';
 const path = require('path');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-    entry: './src/js/lecture_1.js',
+    mode: 'development',
+    entry: './src/js/lecture_1.ts',
     devtool: 'inline-source-map',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.ttf$/,
-				use: ['file-loader']
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                // Taken from: https://webpack.js.org/loaders/sass-loader/
+                test: /\.s(a|c)ss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ttf$/,
+                use: ['file-loader'],
             },
             {
                 test: /\.pug$/,
                 use: {
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
+                    loader: 'pug-loader',
+                    options: {
+                        pretty: true,
+                    },
                 },
             },
-        ]
-	},
-	plugins: [
+        ],
+    },
+    plugins: [
         new MonacoWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./src/lecture_1.pug"
+            template: './src/lecture_1.pug',
         }),
         new CopyWebpackPlugin({
             patterns: [
                 { from: 'src/css', to: './css' },
-                { from: 'src/img', to: './img', }
-            ]
-        })
+                { from: 'src/img', to: './img' },
+            ],
+        }),
     ],
     resolve: {
-        extensions: [".js", ".ts", ".scss", ".css", ".pug"]
+        extensions: ['.js', '.ts', '.scss', 'css', 'sass', '.pug'],
     },
 };
